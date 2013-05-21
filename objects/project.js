3d7fa5513c7c80e3788a06b01e60c4802242a5b0
Julien Calderan <julien.calderan@gmail.com>
2013-05-21 15:50:01 +0300

    Update project.js

--- objects/project.js
+++ objects/project.js
@@ -3,10 +3,10 @@
    "name": "Serious Game",
    "type_projet": "TX",
    "studies_level": "bac+4",
-   "description": "Création d'un serious game tournant à l'aide d'un moteur 3D",
+   "description": "Crï¿½ation d'un serious game tournant ï¿½ l'aide d'un moteur 3D",
    "creation_date": "Thu May 02 2013 09:45:24 GMT+0200",
    "end_date": "Thu May 09 2013 09:45:24 GMT+0200",//new Date(); t.valueOf();
-   "state": "XXXXX",//creation,incubation,campagne,fin
+   "state": 0,// 0 Ã  3 pour creation,incubation,campagne,fin 
    "members": [
        {
            "id_user": "XXXXX",
