aa6dd772ba9c1d5a14b3f2d64548b33b48906696
Julien Calderan <julien.calderan@gmail.com>
2013-05-23 11:22:08 +0300

    Update project.js

--- objects/project.js
+++ objects/project.js
@@ -5,7 +5,8 @@
    "studies_level": "bac+4",
    "description": "Cr�ation d'un serious game tournant � l'aide d'un moteur 3D",
    "creation_date": "Thu May 02 2013 09:45:24 GMT+0200",
-   "end_date": "Thu May 09 2013 09:45:24 GMT+0200",//new Date(); t.valueOf();
+   "end_date": "Thu May 09 2013 09:45:24 GMT+0200",//var t = new Date(); t.valueOf(); date de fin reel
+   "deadline": "Thu May 09 2013 09:45:24 GMT+0200",//date de fin prevue
    "state": 0,// 0 à 3 pour creation,incubation,campagne,fin 
    "members": [
        {
@@ -37,5 +38,9 @@
    "tags":[
       "test",
       "test2"
+   ],
+   "thumbnails":[
+      "http://placehold.it/350x150", //image 1
+      "http://placehold.it/350x150" //image 2
    ]
 }
