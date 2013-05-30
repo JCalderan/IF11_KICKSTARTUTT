90868151d0dec810887929cf9980ca71b423fa37
JCalderan <julien.calderan@gmail.com>
2013-05-30 09:26:50 +0200

    ajout des exemples de document couchdb

--- objects/project.js
+++ objects/project.js
@@ -5,8 +5,7 @@
    "studies_level": "bac+4",
    "description": "Cr�ation d'un serious game tournant � l'aide d'un moteur 3D",
    "creation_date": "Thu May 02 2013 09:45:24 GMT+0200",
-   "end_date": "Thu May 09 2013 09:45:24 GMT+0200",//var t = new Date(); t.valueOf(); date de fin reel
-   "deadline": "Thu May 09 2013 09:45:24 GMT+0200",//date de fin prevue
+   "end_date": "Thu May 09 2013 09:45:24 GMT+0200",//new Date(); t.valueOf();
    "state": 0,// 0 à 3 pour creation,incubation,campagne,fin 
    "members": [
        {
